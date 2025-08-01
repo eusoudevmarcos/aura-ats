831cf64a8602d93783b2538d3f909c5eafd095ea
flmpl <flmp.leonardo@gmail.com>
2025-08-01 08:40:40 -0300

    Update Header component to redirect to login page based on environment
    - Modified the redirection logic in the Header component to direct users to the appropriate login URL for both production and development environments.

--- site/src/components/Header/Header.tsx
+++ site/src/components/Header/Header.tsx
@@ -45,9 +45,9 @@ const Header: React.FC = () => {
     setIsMenuOpen(false);
     // Redireciona para a URL correta dependendo do ambiente
     if (process.env.NODE_ENV === "production") {
-      window.location.href = "https://aura-ats-frontend.vercel.app/";
+      window.location.href = "https://aura-ats-frontend.vercel.app/login";
     } else {
-      window.location.href = "http://localhost:3000";
+      window.location.href = "http://localhost:3000/login";
     }
   };
 
