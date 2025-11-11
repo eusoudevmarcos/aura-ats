728c9fe2152a717910b13c45d08ef5fc260b9435
flmpl <flmp.leonardo@gmail.com>
2025-11-11 14:33:28 -0300

    Remove obsolete migration scripts for various schemas and tables
    - Deleted migration scripts for schemas and tables that are no longer in use, including those for 'Agenda', 'Candidato', 'Plano', and 'Sessao'.
    - Ensured that all related enum types and indexes were also removed to maintain a clean migration history.
    - This cleanup helps streamline the migration process and reduces potential confusion in the database schema.

--- backend/prisma/migrations/20251009141949_add_celular_e_email_candidato/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Candidato"."Candidato" ADD COLUMN     "celular" TEXT,
-ADD COLUMN     "email" TEXT;
--- backend/prisma/migrations/20251028133811_teste_migration_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "PlanoAssinatura"."PlanoAssinatura" ADD COLUMN     "qtdVagas" INTEGER;
